603c31791e1acc685b67c185c0aff15813a50417
xiaolei <xiaolei@shubidatech.com>
2021-12-29 22:59:25 +0800

    Initial commit

---
+++ src/test/java/com/coatardbul/base_server/BaseServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coatardbul.base_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
