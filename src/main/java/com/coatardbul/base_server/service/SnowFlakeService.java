923519ed1181337e0676dbbcb245e27a6727f52f
xiaolei <xiaolei@shubidatech.com>
2021-12-30 17:11:46 +0800

    雪花算法

---
+++ src/main/java/com/coatardbul/base_server/service/SnowFlakeService.java
@@ -0,0 +1,15 @@
+package com.coatardbul.base_server.service;
+
+/**
+ * @author zrj
+ * @date 2021/2/15
+ * @since V1.0
+ **/
+public interface SnowFlakeService {
+    /**
+     * 根据雪花算法获取ID
+     *
+     * @return
+     */
+    String getIdBySnowFlake();
+}
--- src/test/java/com/coatardbul/base_server/BaseServerApplicationTests.java
+++ src/test/java/com/coatardbul/base_server/BaseServerApplicationTests.java
@@ -1,6 +1,6 @@
 package com.coatardbul.base_server;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
